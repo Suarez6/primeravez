111f1d9907b34a6c1621cc38f2024d3feddd88c8
Santiago <s.suarez4@utp.edu.co>
2022-03-16 15:19:09 -0500

    Hice un cambio

--- git2.cpp
+++ git2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int dia, mes, anio;
+
+int main()
+{
+    cout<<"Ingrese el año: ";
+    cin>>anio;
+    if (anio%4 == 0)
+    {
+        cout<<"El año es bisiesto"<<endl;
+    }
+    else if (anio > 1000 && anio%100 == 0 && anio%400 == 0)
+    {
+        cout<<"El año es bisiesto"<<endl;
+    }
+    else
+    {
+        cout<<"El año no es bisiesto"<<endl;
+    }
+}
 
--- git3.cpp
+++ git3.cpp
@@ -1,1 +1,61 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+int dia, mes, anio;
+
+int main()
+{
+    cout<<"Ingrese su dia de nacimiento: ";
+    cin>>dia;
+    cout<<"Ingrese su mes de nacimiento: ";
+    cin>>mes;
+    if ((dia > 20 && mes == 3) || (dia < 20 && mes == 4))
+    {
+        cout<<"Su signo zodiacal es Aries"<<endl;
+    }
+    else if ((dia > 19 && mes == 4) || (dia < 21 && mes == 5))
+    {
+        cout<<"Su signo zodiacal es Tauro"<<endl;
+    }
+    else if ((dia > 20 && mes == 5) || (dia < 21 && mes == 6))
+    {
+        cout<<"Su signo zodiacal es Geminis"<<endl;
+    }
+    else if ((dia > 20 && mes == 6) || (dia < 23 && mes == 7))
+    {
+        cout<<"Su signo zodiacal es Cancer"<<endl;
+    }
+    else if ((dia > 22 && mes == 7) || (dia < 23 && mes == 8))
+    {
+        cout<<"Su signo zodiacal es Leo"<<endl;
+    }
+    else if ((dia > 22 && mes == 8) || (dia < 23 && mes == 9))
+    {
+        cout<<"Su signo zodiacal es Virgo"<<endl;
+    }
+    else if ((dia > 22 && mes == 9) || (dia < 23 && mes == 10))
+    {
+        cout<<"Su signo zodiacal es Libra"<<endl;
+    }
+    else if ((dia > 22 && mes == 10) || (dia < 22 && mes == 11))
+    {
+        cout<<"Su signo zodiacal es Escorpio"<<endl;
+    }
+    else if ((dia > 21 && mes == 11) || (dia < 22 && mes == 12))
+    {
+        cout<<"Su signo zodiacal es Sagitario"<<endl;
+    }
+    else if ((dia > 21 && mes == 12) || (dia < 21 && mes == 1))
+    {
+        cout<<"Su signo zodiacal es Capricornio"<<endl;
+    }
+    else if ((dia > 20 && mes == 1) || (dia < 20 && mes == 2))
+    {
+        cout<<"Su signo zodiacal es Acuario"<<endl;
+    }
+    else if ((dia > 19 && mes == 2) || (dia < 21 && mes == 3))
+    {
+        cout<<"Su signo zodiacal es Piscis"<<endl;
+    }
+}
